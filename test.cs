824358f98771282e4cec9d67e63461db69c2324e
namtaooo <namp1622@gmail.com>
2025-03-16 02:15:58 +0700

    them code moi

---
+++ test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class student
+{
+	private string name { get; set; };
+	private int id { get; set; }
+	private int age { get; set; }
+	private int total { get; set; }
+
+	public student(string Name, int Id, int Age,int Total)
+	{
+		name = Name;
+		id = Id;
+		age = Age;
+		total = Total;
+	}
+	public getInfo()
+	{
+		Console.WriteLine(name);
+        Console.WriteLine(id);
+        Console.WriteLine(age);
+        Console.WriteLine(total);
+
+    }
+}
